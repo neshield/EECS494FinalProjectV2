70a90645776452f10dfd564d953c9d860eb4ed34
Vincent <vjmisovs@umich.edu>
2014-11-15 15:31:20 -0500

    Fixed tunnel logic
    So I fixed the tunnel logic.  Everyone needs to go back and change their
    tunnel objects in order for it to work.
    
    You should:
    1.) Use the new tunnel script
    2.) In the tunnel inspector you need to change the tType to either top,
    bottom, left, or right NOT horizontal/vertical.  Whichever one you
    select will dictate which side of the tunnel obj the object will spawn
    on.

--- FinalProjectV1/Assets/Scripts/TunnelScript.cs
+++ FinalProjectV1/Assets/Scripts/TunnelScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public enum TunnelType {vertical, horizontal};
+public enum TunnelType {vertical, horizontal, top, bottom, left, right};
 
 public class TunnelScript : MonoBehaviour {
 	static bool beenThroughTunnel;
@@ -27,44 +27,108 @@ public class TunnelScript : MonoBehaviour {
 			return;
 		}
 
+		//remove it's current grounds/walls
+		if(pon){
+			pon.resetGround();
+		}
+
+		//position of the object going through the tunnel
 		Vector3 pos = other.transform.position;
-		if(!beenThroughTunnel){
-			//then the y matters where the player goes through
-			if(pairedTunnel.tType == TunnelType.horizontal){
-				pairedTunnel.setSpawnLocation(
-					new Vector3(pairedTunnel.spawnLocation.x, 
-				            	//pon.transform.position.y, 
-				            	pos.y + 0.05f,
-				            	pairedTunnel.spawnLocation.z)
-					);
+
+		BoxCollider bc = other.GetComponent<BoxCollider>();
+
+		//if it uses a box collider
+		if(bc){
+			float buffer = 0.15f;
+			float newX = bc.transform.position.x;
+			float newY = 0.0f;
+
+			//then the paired tunnel is to my left and the spawn location should
+			//be to the right of that one
+			if(pairedTunnel.tType == TunnelType.right){
+				newX = (pairedTunnel.transform.position.x + (pairedTunnel.transform.localScale.x / 2))
+					+ ((other.transform.localScale.x * bc.size.x) / 2) + buffer;
+				//print ("player should be moved to the right of the paired tunnel at x: " + newX);
+
+				//need to figure out the new y.  the paire tunnel may be on a different plane than this one.
+				float pairedTop = pairedTunnel.transform.position.y + (pairedTunnel.transform.localScale.y / 2);
+				//calculates this top and subtracts the position
+				float diffFromTop = (this.transform.position.y + (this.transform.localScale.y / 2)) - other.transform.position.y;
+				newY = pairedTop - diffFromTop;
+
+				pairedTunnel.setSpawnLocation(new Vector3(newX, newY + 0.045f, pairedTunnel.spawnLocation.z));
+
 			}
-			//if it's a vertical then the x direction matters
+			//otherwise it goes to the left
+			else if(pairedTunnel.tType == TunnelType.left){
+				newX = (pairedTunnel.transform.position.x - (pairedTunnel.transform.localScale.x / 2))
+					- ((other.transform.localScale.x * bc.size.x) / 2) - buffer;
+			/*	print ("player should be moved to the left of the paired tunnel at x: " + newX);
+				print ("\tpos.x: " + pairedTunnel.transform.position.x);
+				print ("\tPaired local scale y: " + pairedTunnel.transform.localScale.y);
+				print ("\tPaired local scale x: " + pairedTunnel.transform.localScale.x);
+				print ("\tObj Local scale x: " + other.transform.localScale.x);
+				print ("\tObj box collider size.x: " + bc.size.x);
+				print ("\tBuffer: " + buffer);
+				*/
+				float pairedTop = pairedTunnel.transform.position.y + (pairedTunnel.transform.localScale.y / 2);
+				float diffFromTop = (this.transform.position.y + (this.transform.localScale.y / 2)) - other.transform.position.y;
+				newY = pairedTop - diffFromTop;
+
+				pairedTunnel.setSpawnLocation(new Vector3(newX, newY + 0.045f, pairedTunnel.spawnLocation.z));
+			}
+			else if(pairedTunnel.tType == TunnelType.bottom){
+				newY = (pairedTunnel.transform.position.y - (pairedTunnel.transform.localScale.y / 2)) -
+					(other.transform.localScale.y / 2) - buffer;
+
+				float pairedLeft = pairedTunnel.transform.position.x - (pairedTunnel.transform.localScale.x / 2);
+				float diffFromLeft = other.transform.position.x - (this.transform.position.x - (this.transform.localScale.x / 2));
+				newX = pairedLeft + diffFromLeft;
+
+				pairedTunnel.setSpawnLocation(new Vector3(newX, newY, pairedTunnel.spawnLocation.z));
+
+				print ("setting spawn pos at " + pairedTunnel.spawnLocation);
+			}
+
+			else if(pairedTunnel.tType == TunnelType.top){
+				newY = (pairedTunnel.transform.position.y + (pairedTunnel.transform.localScale.y / 2)) +
+					(other.transform.localScale.y / 2) + buffer;
+
+				float pairedLeft = pairedTunnel.transform.position.x - (pairedTunnel.transform.localScale.x / 2);
+				float diffFromLeft = other.transform.position.x - (this.transform.position.x - (this.transform.localScale.x / 2));
+				newX = pairedLeft + diffFromLeft;
+
+				pairedTunnel.setSpawnLocation(new Vector3(newX, newY, pairedTunnel.spawnLocation.z));
+			}
 			else{
-				float leftSide = this.transform.position.x - (this.transform.localScale.x / 2);
-				//float diff = pon.transform.position.x - Mathf.Abs(leftSide);
-				float diff = pos.x - Mathf.Abs(leftSide);
-				float otherLeft = pairedTunnel.transform.position.x - (pairedTunnel.transform.localScale.x / 2);
-
-				float newX = otherLeft + diff;
-
-
-				pairedTunnel.setSpawnLocation(
-					new Vector3(newX, 
-				            pairedTunnel.spawnLocation.y, 
-				            pairedTunnel.spawnLocation.z)
-					);
+				print ("this isn't supposed to run");
 			}
 
-			//pon.transform.position = pairedTunnel.spawnLocation;
-			other.transform.position = pairedTunnel.spawnLocation;
-			beenThroughTunnel = true;
+
+			
 		}
+	
+
+		/*
+		//if it's a vertical then the x direction matters
 		else{
-			beenThroughTunnel = false;
+			float leftSide = this.transform.position.x - (this.transform.localScale.x / 2);
+			//float diff = pon.transform.position.x - Mathf.Abs(leftSide);
+			float diff = pos.x - Mathf.Abs(leftSide);
+			float otherLeft = pairedTunnel.transform.position.x - (pairedTunnel.transform.localScale.x / 2);
+
+			float newX = otherLeft + diff;
+
+
+			pairedTunnel.setSpawnLocation(
+				new Vector3(newX, 
+			            pairedTunnel.spawnLocation.y, 
+			            pairedTunnel.spawnLocation.z)
+				);
 		}
-
-
-
+		*/
+		other.transform.position = pairedTunnel.spawnLocation;
+		
 	}
 
 	void setSpawnLocation(Vector3 location_){
